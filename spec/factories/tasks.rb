d01508e17b674ca9a4aa4b9e918addf64e587a34
yamasa3150 <itnzsc4b@icloud.com>
2021-01-08 11:33:29 +0900

    Fix: taskのFactoryBotにdeadline追加およびtitle、contentを修正とuserのFactoryBotのemail修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :task do
-    sequence(:title, 'テストタイトル')
-    content {'テスト文'}
+    sequence(:title, "title_1")
+    content { "content" }
     status { :todo }
+    deadline { 1.week.from_now }
     association :user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { |n| "aaa#{n}@bbb.ccc" }
+    sequence(:email) { |n| "user_#{n}@example.com" }
     password { "password" }
     password_confirmation { "password" }
   end
